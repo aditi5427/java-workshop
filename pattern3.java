349bbcefd3a64f9c81b0836689e6eb52f0fd082a
Aditi <aditisuman5480@gmail.com>
2022-05-18 00:18:30 +0530

    pattern files

---
+++ pattern3.java
@@ -0,0 +1,22 @@
+class pattern3 {
+    public static void main (String args[]) {
+    
+        int n = 8;
+      
+        for(int k = 1; k<=9; k++) {
+        for (int l = 1; l<=k ; l++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        
+        for(int i = 1; i<=8; i++) {                                                                        
+            for (int j = 1; j<=n; j++) {
+                System.out.print( "*");
+            } 
+            n--;
+            System.out.println(" ");
+                                                                                        
+        }
+    }
+} 
