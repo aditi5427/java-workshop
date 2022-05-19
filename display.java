e4051c6bd1a4070a46fbd32a65f40362a835de82
Aditi <aditisuman5480@gmail.com>
2022-05-20 00:53:34 +0530

    added files

---
+++ display.java
@@ -0,0 +1,10 @@
+class display {
+    public static void main (String args[]) {
+
+        int arr[] = {2 , 4, 5, 6, 7, 8, 9, 10};
+
+        for(int display:arr) {
+            System.out.print(display + " ");
+        }
+    }
+}
