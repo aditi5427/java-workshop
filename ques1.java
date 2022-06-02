c1e7378a20ed0c75c0b5d82eb8c7cc279a79454b
Aditi <aditisuman5480@gmail.com>
2022-06-02 23:05:32 +0530

    added files

---
+++ ques1.java
@@ -0,0 +1,9 @@
+class ques1 {
+    // Write a program to print numbers from 1 to 10
+    public static void main (String args[]) {
+        int n = 10;
+        for (int i = 1; i <= n; i++ ) {
+            System.out.println(i);
+        }
+    }
+}
